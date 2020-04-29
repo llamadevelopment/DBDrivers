5690c43eb3d34fd114e64263fc50f8474e63f537
Proxma <39440218+ProxmaDev@users.noreply.github.com>
2020-04-29 21:05:43 +0200

    Add files via upload

---
+++ src/main/java/net/lldv/DBDrivers/DBDrivers.java
@@ -0,0 +1,11 @@
+package net.lldv.DBDrivers;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class DBDrivers extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        // Nothing to do.
+    }
+}
